1bf0349bf4650183cb7281921b9159caba47a4fc
Ahnaf-Afif <williamhens123@gmail.com>
2026-02-22 17:52:07 +0600

    done with interview and rejected

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 const all = document.getElementById("allSection");
 const interview = document.getElementById("interviewSection");
 const rejected = document.getElementById("rejectedSection");
+const appliedOrNot = document.getElementById("appliedornot");
 
 function hideShow(id) {
   all.classList.add("hidden");
@@ -18,30 +19,40 @@ function hideShow(id) {
 
 all.addEventListener("click", function (e) {
   if (e.target.classList.contains("interview-btn")) {
-    const card = e.target.closest("#card");
-    interview.appendChild(card);
-    all.removeChild(card);
+    const card = e.target.closest(".cards");
+    card.querySelector(".appliedornot").innerText = "Interview";
+    const clonedCard = card.cloneNode(true);
+    clonedCard.querySelector(".appliedornot").innerText = "Interview";
+    interview.appendChild(clonedCard);
   }
 
   if (e.target.classList.contains("rejected-btn")) {
-    const card = e.target.closest("#card");
-    rejected.appendChild(card);
-    all.removeChild(card);
+    const card = e.target.closest(".cards");
+    card.querySelector(".appliedornot").innerText = "Rejected";
+    const clonedCard = card.cloneNode(true);
+    clonedCard.querySelector(".appliedornot").innerText = "Rejected";
+    rejected.appendChild(clonedCard);
   }
 });
 
 interview.addEventListener("click", function (e) {
   if (e.target.classList.contains("rejected-btn")) {
-    const card = e.target.closest("#card");
-    rejected.appendChild(card);
+    const card = e.target.closest(".cards");
+    card.querySelector(".appliedornot").innerText = "Interview";
+    const clonedCard = card.cloneNode(true);
+    const appliedOrNot1 = card.querySelector(".appliedornot");
+    const appliedOrNot2 = clonedCard.querySelector(".appliedornot");
+    appliedOrNot1.innerText = "Rejected";
+    appliedOrNot2.innerText = "Rejected";
+    rejected.appendChild(clonedCard);
     interview.removeChild(card);
   }
 });
 
 rejected.addEventListener("click", function (e) {
   if (e.target.classList.contains("interview-btn")) {
-    const card = e.target.closest(".card");
+    const card = e.target.closest(".cards");
+    appliedOrNot.innerText = "Interview";
     interview.appendChild(card);
-    rejected.removeChild(card);
   }
 });
